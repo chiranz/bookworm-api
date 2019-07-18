1d0ccc90d03b50ec4138f28f0a8d31e941cd2bd0
chiranz <chiranpoudyal@gmail.com>
2019-07-18 11:48:27 +0530

    Middlewares/server-side Auth with Goodreads API

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,31 +1,56 @@
 import express from "express";
+import authenticate from "../middlewares/authenticate";
+import request from "request-promise";
+import { parseString } from "xml2js";
 
 const router = express.Router();
+router.use(authenticate);
 router.get("/search", (req, res) => {
-  res.json({
-    books: [
-      {
-        goodreadsId: 1,
-        title: "Conman Autobiography",
-        authors: "Chiranjibi",
-        covers: [
-          "https://picsum.photos/id/781/200/200",
-          "https://picsum.photos/id/368/200/200"
-        ],
-        pages: 200
-      },
-      {
-        goodreadsId: 2,
-        title: "Bio of Chiran",
-        authors: "Chiranjibi",
-        covers: [
-          "https://picsum.photos/id/781/200/200",
-          "https://picsum.photos/id/368/200/200"
-        ],
-        pages: 300
-      }
-    ]
-  });
+  request
+    .get(
+      `https://www.goodreads.com/search/index.xml?key=${
+        process.env.GOODREAD_KEY
+      }&q=${req.query.q}`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].image_url[0]]
+            })
+          )
+        })
+      )
+    );
+
+  // res.json({
+  // books: [
+  //   {
+  //     goodreadsId: 1,
+  //     title: "Conman Autobiography",
+  //     authors: "Chiranjibi",
+  //     covers: [
+  //       "https://picsum.photos/id/781/200/200",
+  //       "https://picsum.photos/id/368/200/200"
+  //     ],
+  //     pages: 200
+  //   },
+  //   {
+  //     goodreadsId: 2,
+  //     title: "Bio of Chiran",
+  //     authors: "Chiranjibi",
+  //     covers: [
+  //       "https://picsum.photos/id/781/200/200",
+  //       "https://picsum.photos/id/368/200/200"
+  //     ],
+  //     pages: 300
+  //   }
+  // ]
+  // });
 });
 
 export default router;
